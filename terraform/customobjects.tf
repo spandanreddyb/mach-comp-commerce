df8ed31c144be83af9fcf6b8b3fed92dadb85f7e
Spandanreddy B <spandanreddy.b@valtech.com>
2022-03-28 15:26:32 +0530

    VAL-Intialcommit: intial commit of mach composer

--- terraform/customobjects.tf
+++ terraform/customobjects.tf
@@ -1,5 +1,5 @@
 resource "commercetools_custom_object" "title_custom_object" {
     container = "pumademo-custom-container"
     key = "pumademo-custom-webapp"
-    value = var.variables.homepage_title
+    value = var.variables.HOME_PAGE_TITLE
 }
